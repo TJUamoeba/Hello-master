d04fc5fbf5d9a81b29ec4922f4cca5d47eb568b8
hp <hp@LAPTOP-LLQV7Q0M>
2019-02-28 20:20:32 +0800

    FIRST WERSION

---
+++ Hello/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                System.Console.WriteLine("Hello World!{ 0}is{ 1}", args[0], args[1]);
+            }
+            else
+            {
+                System.Console.WriteLine("Hello World");
+            }
+            Console.BackgroundColor = ConsoleColor.Green;
+            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.WriteLine("Hello World!");
+            Console.ResetColor();
+            System.Console.WriteLine("Hello World");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
+;;
